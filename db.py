d9aad3f49a01c1284f2709e4ed4a8882fb2b4d3d
Grace <117541421+GraceW03@users.noreply.github.com>
2023-11-19 16:01:56 -0500

    [Backend] Finished category model in the database

--- db.py
+++ db.py
@@ -11,4 +11,34 @@ class Category(db.Model):
   name = db.Column(db.String, nullable=False)
   description = db.Column(db.String)
   folder = db.Column(db.String, nullable=False)
-  # flashcards = db.relationship("flashcard", cascade="delete")
+  flashcards = db.relationship("flashcard", cascade="delete")
+
+  def __init__(self, **kwargs):
+    """
+    Initialize a Category Object
+    """
+    self.name = kwargs.get("name")
+    self.description = kwargs.get("description", "")
+    self.folder = kwargs.get("folder")
+
+  def serialize(self):
+    """
+    Serialize a Course Object
+    """
+    return {
+      "id": self.id,
+      "name": self.name,
+      "description": self.description,
+      "foler": self.folder,
+      "flashcards": [f.simple_serialize() for f in self.flashcards]
+    }
+
+  def simple_serialize(self):
+    """
+    Serialize a Category Object with only name and description
+    """
+    return {
+      "id": self.id,
+      "name": self.name,
+      "description": self.description,
+    }
