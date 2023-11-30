84290a37c680a8d900e6bc9ac18c6d30f5587cca
ItsMeTheUser <myab55533@gmail.com>
2023-11-30 16:53:41 -0500

    db with flashcard table added

--- db.py
+++ db.py
@@ -2,11 +2,13 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
 class Category(db.Model):
   """
   Category Model
   """
-  __tablename__ = "task"
+  __tablename__ = "category"
   id = db.Column(db.Integer, primary_key=True, autoincrement=True)
   name = db.Column(db.String, nullable=False)
   description = db.Column(db.String)
@@ -42,3 +44,46 @@ class Category(db.Model):
       "name": self.name,
       "description": self.description,
     }
+  
+
+
+class Flashcard(db.Model):
+  """
+  Flashcard Model
+  """
+  __tablename__ = "flashcard"
+
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  content = db.Column(db.String, nullable=False)
+  answer = db.Column(db.String, nullable=False)
+  category_id = db.Column(db.Integer, db.ForeignKey("category_id"), nullable=False)
+
+
+  def __init__(self, **kwargs):
+        """
+        Initializes a Flashcard Object
+        """
+        self.content = kwargs.get("content")
+        self.answer = kwargs.get("answer")
+        self.category_id = kwargs.get("category_id")
+
+  def serialize(self):
+        """
+        Serialize a Flashcard object
+        """
+        return {
+            "id": self.id,
+            "content": self.content,
+            "answer": self.answer,
+            "category": (Category.query.get(self.category_id)).simple_serialize()
+        }
+  
+  def serialize(self):
+        """
+        Serialize a Flashcard object
+        """
+        return {
+            "id": self.id,
+            "content": self.content,
+            "answer": self.answer
+        }
