d65263e2fea7126047ed4719b55e9edbed280c4e
ItsMeTheUser <myab55533@gmail.com>
2023-11-30 17:38:20 -0500

    flashcard routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import json
+import random
 
 from db import db
 from flask import Flask
@@ -36,11 +37,61 @@ def failure_response(message, code=404):
 
 
 
-app.route("/api/flashcards/", methods=["GET"])
-def get_all_flashcards:
+app.route("/api/flashcards/<int:category_id>", methods=["GET"])
+def get_all_flashcards_in_category(category_id):
     """
-    Endpoint for getting all courses
+    Endpoint for getting all flashcards in a category with category_id
     """
 
-    courses = [course.simple_serialize() for course in Course.query.all()]
-    return success_response({"courses": courses})
+
+app.route("/api/flashcards-random/", methods=["GET"])
+def get_random_flashcard_in_category(category_id):
+    """
+    Endpoint for a random flashcard in a category with category_id
+    """
+    
+    flashcards = category["flashcards"]
+
+    if len(flashcards) == 0:
+        return failure_response("No flashcards in this category")
+    
+    return success_response((random.choice(flashcards)).serialize())
+
+
+app.route("/api/flashcards/<int:category_id>", methods=["POST"])
+def create_flashcard(category_id):
+    """
+    Endpoint for creating a new flashcard in the category category_id
+    """
+
+    try:
+        body = json.loads(request.data)
+        content = body.get("content")
+        answer = body.get("answer")
+        if code == None or body == None:
+            return failure_response("Wrong info provided", 400)
+    except:
+        return failure_response("json fail", 400)
+
+    
+    new_flashcard = Course(content=content, answer=answer)
+    
+    db.session.add(new_flashcard)
+    db.session.commit()
+    return success_response(new_flashcard.serialize(), 201)
+
+
+@app.route("/api/courses/<int:flashcard_id>/", methods=["DELETE"])
+def delete_flashcard(flashcard_id):
+    """
+    Endpoint for deleting the flashcard with id flashcard_id
+    """
+    flashcard = Flashcard.query.filter_by(id=flashcard_id).first()
+    if flashcard is None:
+        return failure_response("Flashcard not found")
+
+    db.session.delete(flashcard)
+    db.session.commit()
+    return success_response(flashcard.serialize())
+
+
