3ce7781e6d5947e6db210773baa2cd6fd91ead21
Grace <117541421+GraceW03@users.noreply.github.com>
2023-11-30 16:57:00 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+
+from db import db
+from flask import Flask
+from flask import request
+import os
+
+# define db filename
+db_filename = "quizrizz.db"
+app = Flask(__name__)
+
+# setup config
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    """
+    Generalized success response function
+    """
+    return json.dumps(data), code
+
+def failure_response(message, code=404):
+    """
+    Generalized failure response function
+    """
+    return json.dumps({"error": message}), code
