50a3d0a5d97a26bc13534303678a9e189ea656a1
DwainA <142837353+DwainA@users.noreply.github.com>
2023-12-01 00:08:09 +0000

    'vc 1 progress'

--- ViewControllers/LogInVC.swift
+++ ViewControllers/LogInVC.swift
@@ -1,1 +1,111 @@
-// sign up could be a subview of login?
+import UIKit
+import SnapKit
+
+
+class LogInVC: UIViewController {
+    //MARK - Properties (view)
+    
+    private let logoIcon = UIImageView() // circular, upper center of the ios device
+    private let appName = UILabel() // directly under the logo, centered on the screen in the scenter 
+    private let appDescripation = UILabel() // directly under app name, the text is also centered
+    private let logInButton = UIButton() // under app descrip 
+    private let signUpButton = UIButton() // under 
+    
+    // MARK - viewDidLoad
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.white
+
+        //helper methods for setting up views
+        setupLogoIcon()
+        setupAppName() // title is Quizz Rizz
+        setupAppDescription()
+        setupLogInButton() // pushing this takes you to the next vc - log in
+        setupSignUpButton() // pushing this takes you to the next 
+        setupPushVCButton() //
+    }
+        
+   
+    }
+    // MARK - Set Up Views
+    private func setupLogoIcon() {
+    logoIcon.image = UIImage(named: "placeholderlogo")
+    logoIcon.contentMode = .scaleAspectFit
+    logoIcon.layer.cornerRadius = logoIcon.frame.width / 2
+    logoIcon.layer.masksToBounds = true
+    
+    view.addSubview(logoIcon)
+    
+    logoIcon.snp.makeConstraints { make in
+        make.centerX.equalToSuperview()
+        make.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(20)
+        make.width.height.equalTo(80)
+    }
+    }
+
+
+    private func setupAppName() {
+        appName.text = "Quizz Rizz"
+        appName.font = UIFont.boldSystemFont(ofSize: 24)
+        view.addSubview(appName)
+        
+        appName.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logoIcon.snp.bottom).offset(20)
+        }
+    }
+
+    private func setupAppDescription() {
+        appDescription.text = "Your app description goes here"
+        appDescription.textAlignment = .center
+        view.addSubview(appDescription)
+        
+        appDescription.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(appName.snp.bottom).offset(10)
+        }
+    }
+
+    private func setupLogInButton() {
+        logInButton.setTitle("Log In", for: .normal)
+        logInButton.setTitleColor(.white, for: .normal)
+        logInButton.backgroundColor = .blue
+        logInButton.addTarget(self, action: #selector(logInButtonTapped), for: .touchUpInside)
+        view.addSubview(logInButton)
+        
+        logInButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(appDescription.snp.bottom).offset(20)
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+        }
+    }
+
+    private func setupSignUpButton() {
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.setTitleColor(.white, for: .normal)
+        signUpButton.backgroundColor = .green
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        view.addSubview(signUpButton)
+        
+        signUpButton.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(logInButton.snp.bottom).offset(10)
+            make.width.equalTo(200)
+            make.height.equalTo(40)
+        }
+    }
+    // MARK: - Button Actions
+
+    @objc private func logInButtonTapped() {
+        
+    }
+
+    @objc private func signUpButtonTapped() {
+        
+    }
+
+
+
+    
+    
