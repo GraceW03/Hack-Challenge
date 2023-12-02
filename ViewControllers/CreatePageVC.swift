08942c1b4c6bace358d9e6134993991692676db2
DwainA <142837353+DwainA@users.noreply.github.com>
2023-12-02 15:26:20 +0000

    /cre

--- ViewControllers/CreatePageVC.swift
+++ ViewControllers/CreatePageVC.swift
@@ -5,35 +5,64 @@
 import UIKit
 import SnapKit
 
-class HomePageVC: UIViewController {
-    
+class CreatePageVC: UIViewController {
     // MARK - Properties (view)
-    private let createFolder = UIButton()
-    private let createSet = UIButton()
+private let createFolder = UIButton()
+private let createSet = UIButton()
 
-    // missing some properties atm
+// Add any missing properties here
 
-    // MARK - viewDidLoad
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.white
+// MARK - viewDidLoad
+override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.white
 
-        
-        setupCreateFolder()
-        setupCreateSet()
-        
+    setupCreateFolder()
+    setupCreateSet()
+}
+
+// MARK - Set Up Views
+
+private func setupCreateFolder() {
+    createFolder.setTitle("Create Folder", for: .normal)
+    createFolder.setTitleColor(.white, for: .normal)
+    createFolder.backgroundColor = .blue
+    createFolder.addTarget(self, action: #selector(createFolderButtonTapped), for: .touchUpInside)
+    view.addSubview(createFolder)
+
+    createFolder.snp.makeConstraints { make in
+        make.centerX.equalToSuperview()
+        make.centerY.equalToSuperview().offset(-40) // Adjust the offset as needed
+        make.width.equalTo(200)
+        make.height.equalTo(40)
     }
+}
 
-    // MARK - Set Up Views
+private func setupCreateSet() {
+    createSet.setTitle("Create Set", for: .normal)
+    createSet.setTitleColor(.white, for: .normal)
+    createSet.backgroundColor = .green
+    createSet.addTarget(self, action: #selector(createSetButtonTapped), for: .touchUpInside)
+    view.addSubview(createSet)
 
-
-    // MARK: - Button Actions
-
-    @objc private func createFolderButtonTapped() {
-        
+    createSet.snp.makeConstraints { make in
+        make.centerX.equalToSuperview()
+        make.centerY.equalToSuperview().offset(40) // Adjust the offset as needed
+        make.width.equalTo(200)
+        make.height.equalTo(40)
     }
+}
 
-    @objc private func createSetButtonTapped() {
-        
-    }
+// MARK: - Button Actions
+
+@objc private func createFolderButtonTapped() {
+    // Implement create folder button action here
+    let createFolderVC = CreateFolderViewController() // Replace with your actual CreateFolderViewController
+    navigationController?.pushViewController(createFolderVC, animated: true)
+}
+
+@objc private func createSetButtonTapped() {
+    // Implement create set button action here
+    let createSetVC = CreateSetViewController() // Replace with your actual CreateSetViewController
+    navigationController?.pushViewController(createSetVC, animated: true)
 }
