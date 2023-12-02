7630ef23c3e3487f9739dc704b09d84c35edeb76
DwainA <142837353+DwainA@users.noreply.github.com>
2023-12-02 17:43:35 +0000

    file chnges

--- ViewControllers/SceneDelegate.swift
+++ ViewControllers/SceneDelegate.swift
@@ -12,24 +12,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // 1. Capture the scene
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        
-        // 2. Create a new UIWindow and pass in a UIWindowScene
-        let window = UIWindow(windowScene: windowScene)
+    // 1. Capture the scene
+    guard let windowScene = (scene as? UIWindowScene) else { return }
 
-        // 3. Create a view hierarchy programmatically
-        let rootVC = ViewController()
-        let navController = UINavigationController(rootViewController: rootVC)
+    // 2. Create a new UIWindow and pass in a UIWindowScene
+    let window = UIWindow(windowScene: windowScene)
 
-        // 4. Set the navigation controller as the window's root view controller
-        window.rootViewController = navController
+    // 3. Create a view hierarchy programmatically
+    let rootVC = InitialPageVC() // Your initial view controller
+    let navController = UINavigationController(rootViewController: rootVC)
 
-        // 5. Set the window and call makeKeyAndVisible()
-        self.window = window
-        window.makeKeyAndVisible()
+    // 4. Set the navigation controller as the window's root view controller
+    window.rootViewController = navController
+
+    // 5. Set the window and call makeKeyAndVisible()
+    self.window = window
+    window.makeKeyAndVisible()
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
