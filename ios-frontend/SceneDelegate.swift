0469056618ee4ec906b8f0a74de2373c4bb6ada2
livnewf <151470051+livnewf@users.noreply.github.com>
2023-12-03 15:05:25 -0500

    Update SceneDelegate.swift

--- ios-frontend/SceneDelegate.swift
+++ ios-frontend/SceneDelegate.swift
@@ -1,9 +1,4 @@
 //
-//  SceneDelegate.swift
-//  A4
-//
-//  Created by Vin Bui on 10/31/23.
-//
 
 import UIKit
 
@@ -12,24 +7,24 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // 1. Capture the scene
-    guard let windowScene = (scene as? UIWindowScene) else { return }
+        // 1. Capture the scene
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // 2. Create a new UIWindow and pass in a UIWindowScene
+        let window = UIWindow(windowScene: windowScene)
 
-    // 2. Create a new UIWindow and pass in a UIWindowScene
-    let window = UIWindow(windowScene: windowScene)
+        // 3. Create a view hierarchy programmatically
+        let rootVC = InitialPageVC()
+        let navController = UINavigationController(rootViewController: rootVC)
 
-    // 3. Create a view hierarchy programmatically
-    let rootVC = InitialPageVC()  
-    let navController = UINavigationController(rootViewController: rootVC)
+        // 4. Set the navigation controller as the window's root view controller
+        window.rootViewController = navController
 
-    // 4. Set the navigation controller as the window's root view controller
-    window.rootViewController = navController
-
-    // 5. Set the window and call makeKeyAndVisible()
-    self.window = window
-    window.makeKeyAndVisible()
+        // 5. Set the window and call makeKeyAndVisible()
+        self.window = window
+        window.makeKeyAndVisible()
     }
-    
+
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
