f2b980544e01d5ef6566f825a01ffa2b785e9c5a
livnewf <151470051+livnewf@users.noreply.github.com>
2023-12-03 19:28:01 -0500

    Update CardsInSetVC.swift

--- ios-frontend/CardsInSetVC.swift
+++ ios-frontend/CardsInSetVC.swift
@@ -1,90 +1,73 @@
 import Foundation
-import UIKit
 import SnapKit
+import UIKit
 
-class CardsInSetVC : UIViewController {
-
-weak var expandSetDelegate : ExpandSet?
-var cards : [Card] = []
-private let setName = UILabel()
-private let photo = UIImageView(image : UIImage(named: placeholderlogo))
-private let cardCollectionView = UICollectionView()
-private let studyButton = UIButton()
-
-    init(set: SetFolder, delegate: ExpandSet) {
-        self.cards = sett.cards
-        self.name = sett.name
-        self.expandSetDelegate = delegate
-        super.init(nibName: nil, bundle: nil)
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-
-        view.backgroundColor = UIColor.white
-        title = setName
-        navigationController?.navigationBar.prefersLargeTitles = true
-        
-        setupCards()
-    }
-
-    private func setupCards() {
-
-      let layout = UICollectionViewFlowLayout()
-      layout.scrollDirection = .vertical
-
-      cardCollectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-      cardCollectionView.alwaysBounceVertical = true			
-	    cardCollectionView.register(CardCell.self, forCellWithReuseIdentifier: CardCell.reuse) // 5
-	    cardCollectionView.delegate = self 
-	    cardCollectionView.dataSource = self 
-		
-	    view.addSubview(cardCollectionView)
-
-      cardCollectionView.snp.makeConstraints { make in
-          make.leading.equalToSuperview().offset(55)
-          make.top.equalToSuperview().offset(129)
-          make.trailing.equalToSuperview().offset(-55)
-          make.bottom.equalToSuperview().offset(-201)
-      }
-
-    }
-
+protocol FillSet : AnyObject {
+  func createItem(term : String, definition : String)
 }
 
-extension CardsInSetVC: UICollectionViewDelegate {
+class CreateSetVC: UIViewController {
+  
+  //MARK - Properties (view)
+  let private folderName = UILabel()
+  let private userName = UILabel()
+  let private termCount = UILabel()
+  let private createButton = UIButton()
+  let private termField = UITextField()
+  let private descriptionField = UITextField()
 
-}
+  //MARK - PROPERTIES (DATA)
+  private var term : String = ""
+  private var definition : String = ""
+  private weak var delegate : SetFolderName?
+   
+  // MARK - viewDidLoad
+  override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = UIColor.white
 
-extension CardsInSetVC: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let rec = cardCollectionView.dequeueReusableCell(withReuseIdentifier: CardCell.reuse, for: indexPath) as? 
-    CardCell 
-    else { return UICollectionViewCell() }
-      let card = cards[indexPath.item]
-      rec.configure(set: sett)
-      return rec
+      setupCategory()
+      //helper methods for setting up views
+  }
+    
+
+  // MARK - Set Up Views
+  private func setupCategory() {
+  
+  view.addSubview(folderImage)
+  view.addSubview(folderName)
+  view.addSubview(termCount)
+  view.addSubview(createButton)
+
+  folderImage.snp.makeConstraints = { make in
+          make.leading.equalToSuperview().offset(124)
+          make.top.equalToSuperview().offset(183)
+          make.trailing.equalToSuperview().offset(-114)
+          make.bottom.equalToSuperview().offset(-522)
+        }
+
+  folderName.snp.makeConstraints = { make in
+          make.leading.equalToSuperview().offset(144)
+          make.top.equalToSuperview().offset(300)
+          make.trailing.equalToSuperview().offset(-135)
+          make.bottom.equalToSuperview().offset(-463)
+        }
   }
 
-  func collectionView(_ collectionView: UICollectionView, 
-  numberOfItemsInSection section: Int) -> Int {
-    return cards.count
+  // MARK: - Button Actions
+
+  @objc private func pushSet() {
+    let insideFolderVC = InsideFolderVC(text : text, delegate : self) 
+    navigationController?.pushViewController(finalFolderVC, animated: true)
+      delegate?.createItem(term : termField.text ?? "", definition: descriptionField.text ?? "")
+      NetworkManager.shared.createCategory(categoryID: id, content: term, answer: definition) { success in
+            if success {
+                let insideFolderVC = InsideFolderVC(text : text, delegate : self) 
+    navigationController?.pushViewController(finalFolderVC, animated: true)
+            } else {
+                // Handle signup failure, show an error message, etc.
+            }
+        }
   }
 
-} 
-
-extension CardsInSetVC: UICollectionViewDelegateFlowLayout {
-    // `sizeForItemAt`
-    // Additional functions here
-    let collectionViewWidth = collectionView.bounds.width
-    let cellPadding: CGFloat = 24
-    let cellWidth = (collectionViewWidth - cellPadding) / 2
-    let cellHeight: CGFloat = 216
-            
-    return CGSize(width: cellWidth, height: cellHeight)
 }
